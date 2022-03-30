852cca8e9327eb02749f5038ddf10777ad030bda
sahop <pratap.sahoo@in.pega.com>
2022-03-30 23:25:56 +0530

    Added Heaps - kthSmallestMatrix,findKthSmallest,mergeKLists

--- src/Heaps/SortedArray.java
+++ src/Heaps/SortedArray.java
@@ -66,19 +66,138 @@ public class SortedArray {
       Merge k Sorted Lists - Given an array of k linked-lists lists, each linked-list is sorted in ascending order.
       Merge all the linked-lists into one sorted linked-list and return it.
       https://leetcode.com/problems/merge-k-sorted-lists/
+      We can use minHeap to solve this -
+      1. Insert the first element of each array in a Min Heap.
+      2. Remove the smallest (top) element from the heap and add it to the merged list.
+      3. Insert the next element of the same list into the heap - Removed node will already contain its next node address.
      */
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode mergeKLists(ListNode[] lists) {
+        ListNode solutionHead =null, solutionTail = null;
+        //minHeap to store ListNode - It's effective to store node data itself as it contains next node address
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b) -> a.val - b.val);
+
+        //Add root of each list , if not null
+        for(ListNode root : lists){
+            if(root != null) {
+                minHeap.add(root);
+            }
+        }
+
+        while (!minHeap.isEmpty()){
+            //Remove minHeap Top
+            ListNode currNode = minHeap.poll();
+
+            // First element in merged list
+            if(solutionHead == null){
+                solutionHead = solutionTail = currNode;
+            }
+            else {
+                solutionTail.next = currNode;
+                solutionTail = currNode;
+            }
+            //Insert the next element of the same list into the minHeap.
+            if(currNode.next != null) minHeap.add(currNode.next);
+        }
+
+        return solutionHead;
+    }
 
     /*
     Kth Smallest Number in M Sorted Lists - Given ‘M’ sorted arrays, find the K’th smallest number among all the arrays.
     https://www.geeksforgeeks.org/find-m-th-smallest-value-in-k-sorted-arrays/
+    We can solve this using a minHeap : Just like mergeKLists, we will merge all sorted arrays until we have removed
+    K elements from heap. Idea of using a minHeap instead of maxHeap here is that even before adding all elements we can get our solution,
+    if it were list of non-sorted arrays, maxHeap would have made sense.
+    But here the input is a list of arrays compared to LinkedLists.
+    This means that when we want to push the next number in the heap we need to know what the index of the current number in the current array was.
+    To handle this, we will need to keep track of the array and the element indices.
      */
+    public static int findKthSmallest(List<Integer[]> lists, int K) {
+        //minHeap with element as :
+        //int[0] = number , int[1] = elementIndex, int[2] = arrayIndex
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        //Adding first element from each list
+        for (int list = 0; list < lists.size(); list++) {
+            if (lists.get(list) != null) {
+                minHeap.add(new int[]{lists.get(list)[0], 0, list});
+            }
+        }
+
+        int numCount = 0, solution = 0;
+        while (!minHeap.isEmpty()){
+            //Poll top element & assigned it to solution
+            int[] currNum = minHeap.poll();
+            solution = currNum[0];
+            //Have we already popped K elements ? If yes, this solution is our Kth smallest element, break from this loop
+            if(++numCount == K) break;
+
+            //Increment index of current array from lists
+            currNum[1] += 1;
+            //If current index is less than length of that array, add next element in that array
+            if(lists.get(currNum[2]).length > currNum[1]) {
+                minHeap.add(new int[]{lists.get(currNum[2])[currNum[1]], currNum[1], currNum[2]});
+            }
+        }
+
+        return solution;
+    }
+
+
 
 
     /*
     Kth Smallest Element in a Sorted Matrix - Given an n x n matrix where each of the rows and columns is sorted in ascending order,
     return the kth smallest element in the matrix.
     https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+    Heap solution will be exactly same as findKthSmallest problem, just instead of list of arrays, we got a matrix.
+    Here as the rows and columns are sorted, we can use a Binary-Search solution too which is more efficient,
+    will be discussing that in Binary Search patterns
      */
+    public static int kthSmallestMatrix(int[][] matrix, int K) {
+        //minHeap with element as :
+        //int[0] = number , int[1] = row, int[2] = column
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        //Adding first element from each list
+        //We are adding row < K, as rows are sorted and if K is less than number of rows, our solution will never lie on rows greater than K
+        for (int row = 0; row < matrix.length && row < K; row++) {
+            minHeap.add(new int[]{matrix[row][0], row, 0});
+        }
+
+        int numCount = 0, solution = 0;
+        while (!minHeap.isEmpty()){
+            //Poll top element & assigned it to solution
+            int[] currNum = minHeap.poll();
+            solution = currNum[0];
+            //Have we already popped K elements ? If yes, this solution is our Kth smallest element, break from this loop
+            if(++numCount == K) break;
+
+            //Increment index of current array from lists
+            currNum[2] += 1;
+            //If current index is less than length of that array, add next element in that array
+            if(matrix[currNum[1]].length > currNum[2]) {
+                minHeap.add(new int[]{matrix[currNum[1]][currNum[2]], currNum[1], currNum[2]});
+            }
+        }
+
+        return solution;
+    }
 
     /*
     Smallest Range Covering Elements from K Lists - You have k lists of sorted integers in non-decreasing order.
